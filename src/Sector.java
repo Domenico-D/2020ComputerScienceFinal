88b58e008af5aa69ace8d3a084eee7cf1899b538
Domenico-D <55961807+Domenico-D@users.noreply.github.com>
2020-01-19 18:09:21 -0500

    Sector updated
    Sector updated

--- src/Sector.java
+++ src/Sector.java
@@ -1,2 +1,55 @@
 public class Sector {
+
+    private String name;
+    private double sectorProfitLoss;
+    private char sectorVolatality;
+    private Stock[] instruments;
+
+    public Sector(String nm, Stock[] stocks){
+
+        name = nm;
+        instruments = stocks;
+
+    }
+
+    public void sectorVolatility(){
+
+        //Constants to store volatility levels to determine char
+        final double high = 10;
+        final double medium = 20;
+        final double low = 30;
+
+        //For loop to add up all of the volatility of each stock in the sector
+        int volatilityTotal = 0;
+
+        for(int i = 0; i < instruments.length; i++){
+
+            volatilityTotal += instruments[i].getVolability;
+
+        }
+
+        //Get average volatility
+        double vAverage = volatilityTotal / instruments.length;
+
+        //Check how high volatility is and assign it a char based on average velocity
+        if(vAverage >= high){
+            sectorVolatality = 'H';
+        }
+        else if(vAverage >= medium){
+            sectorVolatality = 'M';
+        }
+        else{
+            sectorVolatality = 'L';
+        }
+    }
+
+    public void get
+
+    public double getSectorProfitLoss() {
+        return sectorProfitLoss;
+    }
+
+    public char getSectorVolatality() {
+        return sectorVolatality;
+    }
 }
