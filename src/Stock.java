84e95deb4113a74e6c137f2f32b24d9a51e166f9
huynhj02 <60010751+huynhj02@users.noreply.github.com>
2020-01-19 18:22:05 -0500

    Update Stock.java

--- src/Stock.java
+++ src/Stock.java
@@ -4,7 +4,7 @@
  */
 
 public class Stock {
-//Jimmy is fried
+
     private String name;
     private char region;
     private int sharesOwned;
@@ -15,7 +15,6 @@ public class Stock {
 
     public Stock(String nm, char location, double goingPrice, double risk, boolean openMarket)
     {
-        //dwfafwafsafwafs
         name = nm;
         region = location;
         currentPrice = goingPrice;
@@ -76,20 +75,4 @@ public class Stock {
         sharesOwned -= sell;
     }
 
-    public void ddyes()
-    {
-
-    }
-
-    // dd is yes
-
-    public void pizzatime()
-    {
-        System.out.println("goog pizza");
-    }
-
-    public void test(){
-        System.out.println("hello");
-        System.out.println("JIMMY WAS HERE");
-    }
 }
