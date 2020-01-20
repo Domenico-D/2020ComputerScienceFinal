a9a8baf61ed6dea7646f7bd102fed11f80797dc6
huynhj02 <60010751+huynhj02@users.noreply.github.com>
2020-01-19 21:24:43 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+/**
+ * Domenico Didiano and Jimmy Huynh
+ * Date: January, 24, 2020
+ * ICS 4U1, ISU --- Stonks.
+ * A program which mimics the stock market, allowing you to trade,
+ * create artificial stocks, and trade with fake money.
+ */
+
+import java.util.*;
+import java.lang.*;
+
 public class Main {
 
     public static void main(String[]args){
-        System.out.println("hello");
+
+        Scanner inputStr = new Scanner(System.in);
+        Scanner inputNum = new Scanner(System.in);
+
+        while()
+
 
 
         /**
