d50f6aaf740b9fc120ea767f885f6ba9e1312fa4
Domenico-D <55961807+Domenico-D@users.noreply.github.com>
2020-01-21 22:26:20 -0500

    Updated Stock prices and risk

--- src/Main.java
+++ src/Main.java
@@ -108,14 +108,14 @@ public class Main {
     private static Stock userPortfolio3Sector5Stock5 = new Stock();
 
     //All Technology Sector Stocks
-    private static Stock intelStock = new Stock("Intel", 'A', 59.41, 8.8, true);
-    private static Stock appleStock = new Stock("Apple", 'A', 59.41, 8.8, true);
-    private static Stock alphabetStock = new Stock("Alphabet", 'A', 59.41, 8.8, true);
-    private static Stock samsungStock = new Stock("Samsung", 'A', 59.41, 8.8, true);
-    private static Stock microsoftStock = new Stock("Microsoft", 'A', 59.41, 8.8, true);
-    private static Stock nvidiaStock = new Stock("Nvidia", 'A', 59.41, 8.8, true);
-    private static Stock nintendoStock = new Stock("Nintendo", 'A', 59.41, 8.8, true);
-    private static Stock amdStock = new Stock("AMD", 'A', 59.41, 8.8, true);
+    private static Stock intelStock = new Stock("Intel", 'N', 60.54, 0.7, true);
+    private static Stock appleStock = new Stock("Apple", 'N', 316.75, 0.8, true);
+    private static Stock alphabetStock = new Stock("Alphabet", 'N', 482.98, 0.9, true);
+    private static Stock samsungStock = new Stock("Samsung", 'A', 218.32, 0.7, true);
+    private static Stock microsoftStock = new Stock("Microsoft", 'N', 166.25, 0.8, true);
+    private static Stock nvidiaStock = new Stock("Nvidia", 'A', 247.95, 0.1, true);
+    private static Stock nintendoStock = new Stock("Nintendo", 'A', 53.26, 0.7, true);
+    private static Stock amdStock = new Stock("AMD", 'A', 51.05, 0.1, true);
 
     //Array of all technology stocks
     private static Stock[] technologyArray = {intelStock, appleStock, alphabetStock, samsungStock, microsoftStock, nvidiaStock, nintendoStock, amdStock};
@@ -124,14 +124,14 @@ public class Main {
     private static Sector technologySector = new Sector("Technology", technologyArray);
 
     //All Crypto Currency Stocks
-    private static Stock bitcoinStock = new Stock("Bitcoin", 'A', 59.41, 8.8, true);
-    private static Stock doge_CoinStock = new Stock("Doge Coin", 'A', 59.41, 8.8, true);
-    private static Stock litecoinStock = new Stock("Litecoin", 'A', 59.41, 8.8, true);
-    private static Stock zCashStock = new Stock("zCash", 'A', 59.41, 8.8, true);
-    private static Stock ethereumStock = new Stock("Ethereum", 'A', 59.41, 8.8, true);
-    private static Stock zuccBuccStock = new Stock("ZuccBucc", 'A', 59.41, 8.8, true);
-    private static Stock rippleStock = new Stock("Ripple", 'A', 59.41, 8.8, true);
-    private static Stock dashStock = new Stock("Dash", 'A', 59.41, 8.8, true);
+    private static Stock bitcoinStock = new Stock("Bitcoin", 'N', 8700.32, 0.17, true);
+    private static Stock doge_CoinStock = new Stock("Doge Coin", 'E', 1.37, 0.13, true);
+    private static Stock litecoinStock = new Stock("Litecoin", 'A', 57.31, 0.16, true);
+    private static Stock zCashStock = new Stock("zCash", 'F', 53.23, 0.1, true);
+    private static Stock ethereumStock = new Stock("Ethereum", 'O', 168.19, 0.15, true);
+    private static Stock zuccBuccStock = new Stock("ZuccBucc", 'N', 666.66, 0.2, true);
+    private static Stock rippleStock = new Stock("Ripple", 'O', 1.23, 0.11, true);
+    private static Stock dashStock = new Stock("Dash", 'F', 108.13, 0.14, true);
 
     //Array of crypto currency stocks
     private static Stock[] cryptoArray = {bitcoinStock, doge_CoinStock, litecoinStock, zCashStock, ethereumStock, zuccBuccStock, rippleStock, dashStock};
@@ -140,14 +140,14 @@ public class Main {
     private static Sector cryptoCurrencySector = new Sector("Crypto Currency", cryptoArray);
 
     //All Retail Stocks
-    private static Stock amazonStock = new Stock("Amazon", 'A', 59.41, 8.8, true);
-    private static Stock alibabaStock = new Stock("Alibaba", 'A', 59.41, 8.8, true);
-    private static Stock bestBuyStock = new Stock("BestBuy", 'A', 59.41, 8.8, true);
-    private static Stock costcoStock = new Stock("Costco", 'A', 59.41, 8.8, true);
-    private static Stock rexallStock = new Stock("Rexall", 'A', 59.41, 8.8, true);
-    private static Stock wishStock = new Stock("Wish", 'A', 59.41, 8.8, true);
-    private static Stock freshCoStock = new Stock("FreshCo", 'A', 59.41, 8.8, true);
-    private static Stock ebayStock = new Stock("Ebay", 'A', 59.41, 8.8, true);
+    private static Stock amazonStock = new Stock("Amazon", 'N', 1892.02, 0.09, true);
+    private static Stock alibabaStock = new Stock("Alibaba", 'A', 222.26, 0.09, true);
+    private static Stock bestBuyStock = new Stock("BestBuy", 'N', 90.24, 0.07, true);
+    private static Stock costcoStock = new Stock("Costco", 'N', 313.26, 0.07, true);
+    private static Stock rexallStock = new Stock("Rexall", 'N', 1329.28, 0.09, true);
+    private static Stock wishStock = new Stock("Wish", 'A', 841.42, 0.08, true);
+    private static Stock freshCoStock = new Stock("FreshCo", 'N', 64.82, 0.07, true);
+    private static Stock ebayStock = new Stock("Ebay", 'N',35.72 , 0.08, true);
 
     //Array of retail stocks
     private static Stock[] retailArray = {amazonStock, alibabaStock, bestBuyStock, costcoStock, rexallStock, wishStock, freshCoStock, ebayStock};
@@ -156,14 +156,14 @@ public class Main {
     private static Sector retailSector = new Sector("Retail", retailArray);
 
     //All auto Stocks
-    private static Stock fordStock = new Stock("Ford", 'A', 59.41, 8.8, true);
-    private static Stock volkswagenStock = new Stock("Volkswagen", 'A', 59.41, 8.8, true);
-    private static Stock hondaStock = new Stock("Honda", 'A', 59.41, 8.8, true);
-    private static Stock toyotaStock = new Stock("Toyota", 'A', 59.41, 8.8, true);
-    private static Stock teslaStock = new Stock("Tesla", 'A', 59.41, 8.8, true);
-    private static Stock volvoStock = new Stock("Volvo", 'A', 59.41, 8.8, true);
-    private static Stock nissanStock = new Stock("Nissan", 'A', 59.41, 8.8, true);
-    private static Stock ferrariStock = new Stock("Ferrari", 'A', 59.41, 8.8, true);
+    private static Stock fordStock = new Stock("Ford", 'N', 9.21, 0.04, true);
+    private static Stock volkswagenStock = new Stock("Volkswagen", 'E', 181.30, 0.06, true);
+    private static Stock hondaStock = new Stock("Honda", 'A', 89.41, 0.03, true);
+    private static Stock toyotaStock = new Stock("Toyota", 'A', 32.45, 0.04, true);
+    private static Stock teslaStock = new Stock("Tesla", 'N', 523.89, 0.06, true);
+    private static Stock volvoStock = new Stock("Volvo", 'E', 161.45, 0.05, true);
+    private static Stock nissanStock = new Stock("Nissan", 'A', 29.41, 0.03, true);
+    private static Stock ferrariStock = new Stock("Ferrari", 'E', 213.13, 0.06, true);
 
     //Array of auto Stocks
     private static Stock[] autoArray = {fordStock, volkswagenStock, hondaStock, toyotaStock, teslaStock, volvoStock, nissanStock, ferrariStock};
@@ -172,30 +172,30 @@ public class Main {
     private static Sector autoSector = new Sector("Auto", autoArray);
 
     //All Aerospace and Defense Stocks
-    private static Stock bombardierStock = new Stock("Bombardier", 'A', 59.41, 8.8, true);
-    private static Stock generalElectricStock = new Stock("General Electric", 'A', 59.41, 8.8, true);
-    private static Stock boeingStock = new Stock("Boeing", 'A', 59.41, 8.8, true);
-    private static Stock unitedTechnologiesStock = new Stock("United Technologies", 'A', 59.41, 8.8, true);
-    private static Stock airbusStock = new Stock("Airbus", 'A', 59.41, 8.8, true);
-    private static Stock honeywellStock = new Stock("Honeywell", 'A', 59.41, 8.8, true);
-    private static Stock spaceXStock = new Stock("SpaceX", 'A', 59.41, 8.8, true);
-    private static Stock safronStock = new Stock("Safron", 'A', 59.41, 8.8, true);
+    private static Stock bombardierStock = new Stock("Bombardier", 'A', 1.29, 0.07, true);
+    private static Stock generalElectricStock = new Stock("General Electric", 'A', 11.66, 0.08, true);
+    private static Stock boeingStock = new Stock("Boeing", 'A', 313.37, 0.09, true);
+    private static Stock unitedTechnologiesStock = new Stock("United Technologies", 'A', 59.41, 0.07, true);
+    private static Stock airbusStock = new Stock("Airbus", 'A', 136.41, 0.08, true);
+    private static Stock honeywellStock = new Stock("Honeywell", 'A', 180.93, 0.08, true);
+    private static Stock spaceXStock = new Stock("SpaceX", 'A', 132.56, 0.09, true);
+    private static Stock safranStock = new Stock("Safran", 'A', 142.00, 0.09, true);
 
     //Array of Aerospace and defence stocks
-    private static Stock[] aeroArray = {bombardierStock, generalElectricStock, boeingStock, unitedTechnologiesStock, airbusStock, honeywellStock, spaceXStock, safronStock};
+    private static Stock[] aeroArray = {bombardierStock, generalElectricStock, boeingStock, unitedTechnologiesStock, airbusStock, honeywellStock, spaceXStock, safranStock};
 
     //Aerospace Sector
     private static Sector aerospaceSector = new Sector("Aerospace", aeroArray);
 
     //All Financial / banks stocks
-    private static Stock scotiaBankStock = new Stock("ScotiaBank", 'A', 59.41, 8.8, true);
-    private static Stock cibcStock = new Stock("CIBC", 'A', 59.41, 8.8, true);
-    private static Stock bankOfAmericaStock = new Stock("Bank of America", 'A', 59.41, 8.8, true);
-    private static Stock bmoStock = new Stock("BMO", 'A', 59.41, 8.8, true);
-    private static Stock hAndRBlockStock = new Stock("H and R Block", 'A', 59.41, 8.8, true);
-    private static Stock tdStock = new Stock("TD", 'A', 59.41, 8.8, true);
-    private static Stock rbcStock = new Stock("RBC", 'A', 59.41, 8.8, true);
-    private static Stock tangerineStock = new Stock("Tangerine", 'A', 59.41, 8.8, true);
+    private static Stock scotiaBankStock = new Stock("ScotiaBank", 'N', 72.63, 0.05, true);
+    private static Stock cibcStock = new Stock("CIBC", 'N', 108.14, 0.07, true);
+    private static Stock bankOfAmericaStock = new Stock("Bank of America", 'N', 34.26, 0.03, true);
+    private static Stock bmoStock = new Stock("BMO", 'N', 104.34, 0.04, true);
+    private static Stock hAndRBlockStock = new Stock("H and R Block", 'E', 53.63, 8.8, true);
+    private static Stock tdStock = new Stock("TD", 'N', 73.96, 0.05, true);
+    private static Stock rbcStock = new Stock("RBC", 'N', 106.79, 0.02, true);
+    private static Stock tangerineStock = new Stock("Tangerine", 'O', 163.76, 0.06, true);
 
     //Array of Financial stocks
     private static Stock[] financialArray = {scotiaBankStock, cibcStock, bankOfAmericaStock, bmoStock, hAndRBlockStock, tdStock, rbcStock, tangerineStock};
@@ -204,17 +204,17 @@ public class Main {
     private static Sector financialSector = new Sector("Financial", financialArray);
 
     //All food Stocks
-    private static Stock mcDonaldsStock = new Stock("McDonalds", 'A', 59.41, 8.8, true);
-    private static Stock wendysStock = new Stock("Wendys", 'A', 59.41, 8.8, true);
-    private static Stock chipotleStock = new Stock("Chipotle", 'A', 59.41, 8.8, true);
-    private static Stock dominoesStock = new Stock("Dominoes", 'A', 59.41, 8.8, true);
-    private static Stock beyondMeatStock = new Stock("BeyondMeat", 'A', 59.41, 8.8, true);
-    private static Stock pepsiCoStock = new Stock("PepsiCo", 'A', 59.41, 8.8, true);
-    private static Stock heinekenStock = new Stock("Heineken", 'A', 59.41, 8.8, true);
-    private static Stock henneseyStock = new Stock("Hennesey", 'A', 59.41, 8.8, true);
+    private static Stock mcDonaldsStock = new Stock("McDonalds", 'A', 211.16, 0.08, true);
+    private static Stock wendysStock = new Stock("Wendys", 'A', 21.66, 0.09, true);
+    private static Stock chipotleStock = new Stock("Chipotle", 'A', 870.73, 0.07, true);
+    private static Stock dominosStock = new Stock("Dominos", 'A', 285.11, 0.05, true);
+    private static Stock beyondMeatStock = new Stock("BeyondMeat", 'A', 129.18, 0.07, true);
+    private static Stock pepsiCoStock = new Stock("PepsiCo", 'A', 141.86, 0.1, true);
+    private static Stock heinekenStock = new Stock("Heineken", 'A', 99.86, 0.07, true);
+    private static Stock henneseyStock = new Stock("Hennesey", 'A', 51.41, 0.12, true);
 
     //Array of food Stocks
-    private static Stock[] foodArray = {mcDonaldsStock, wendysStock, chipotleStock, dominoesStock, beyondMeatStock, pepsiCoStock, heinekenStock, henneseyStock};
+    private static Stock[] foodArray = {mcDonaldsStock, wendysStock, chipotleStock, dominosStock, beyondMeatStock, pepsiCoStock, heinekenStock, henneseyStock};
 
     //Food Sector
     private static Sector foodSector = new Sector("Food", foodArray);
@@ -279,13 +279,11 @@ public class Main {
 
         int choice, gameChoice;
 
-        System.out.println(technologySector.toString());
-
-
         do{
-
+            System.out.println("[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]\n");
             System.out.println("1. Play");
-            System.out.println("2. About us");
+            System.out.println("2. About us\n");
+            System.out.println("[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]");
             choice = inputNum.nextInt();
 
             if(choice == 1){
@@ -297,9 +295,11 @@ public class Main {
                         gameChoice = 1;
                     }
                     else{
+                        System.out.println("[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]\n");
                         System.out.println("\n1. Create a Portfolio");
                         //Only allow user to select Portfolios if they have more then one
                         System.out.println("2. Select Portfolio");
+                        System.out.println("[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]");
                         gameChoice = inputNum.nextInt();
                     }
 
@@ -311,9 +311,9 @@ public class Main {
 
                     }
                     else if(gameChoice == 2){
-
+                        System.out.println("[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]\n");
                         System.out.println("\nWhich portfolio would you like to access");
-
+                        System.out.println("[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅][̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]");
                     }
 
                 }while(gameChoice != 0);
@@ -375,8 +375,10 @@ public class Main {
 
                                 //Check if they want to create or choose a stock
                                 if(choice == 1){
+                                    System.out.println("\n\n");
+                                    System.out.format("%-25s %4s %8s %4s %6s","Name","Region","Price","Volatility","Active" + "\n");
                                     System.out.println(technologySector.toString());
-                                    System.out.print("\nWhich stock would you like to add: ");
+                                    System.out.print("Which stock would you like to add: ");
                                     stockChoice = inputNum.nextInt();
 
                                     if(stockChoice == 1 && chosenStock[0]){
@@ -431,6 +433,7 @@ public class Main {
                                 else if(choice == 2){
 
                                     if(o == 0){
+
                                         System.out.print("\nEnter the name of your stock");
                                         userPortfolio1Sector1Stock1.setName(inputStr.nextLine());
 
